449cb7a956665a0a9c9d53be490f408665c997eb
Yohann <73314529+yohann69@users.noreply.github.com>
2023-12-14 16:12:27 +0100

    Initial commit

---
+++ VoyoBackend.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	//app.Use(cors.New(cors.Config{
+	//	AllowOrigins: "*",
+	//	AllowMethods: "GET",
+	//	AllowHeaders: "Content-Type",
+	//}))
+
+	app.Get("/", myfunction)
+
+	app.Get("/status", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	app.Get("/version", func(c *fiber.Ctx) error {
+		return c.SendString("1.0.0")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
